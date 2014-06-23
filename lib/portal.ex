c4c2b4b645f9ffaddbaf4e84a4e7cc2d7a3e6461
José Valim <jose.valim@plataformatec.com.br>
2014-06-23 13:46:48 +0200

    Portal app all done

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,54 +1,64 @@
 defmodule Portal do
   use Application
 
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec
-    children = [worker(Portal.Hole, [])]
+    children = [worker(Portal.Door, [])]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
     opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
+  @doc """
+  Shoots a new door with the given `color`.
+  """
   def shoot(color) do
     Supervisor.start_child(Portal.Supervisor, [color])
   end
 
   defstruct [:left, :right]
 
+  @doc """
+  Starts transfering `data` from `left` to `right`.
+  """
   def transfer(left, right, data) do
     for item <- data do
-      Portal.Hole.push(left, item)
+      Portal.Door.push(left, item)
     end
     %Portal{left: left, right: right}
   end
 
-  def push_right(%Portal{left: left, right: right}) do
-    case Portal.Hole.pop(left) do
+  @doc """
+  Pushes data to the right in the given `portal`.
+  """
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
       :error   -> :ok
-      {:ok, h} -> Portal.Hole.push(right, h)
+      {:ok, h} -> Portal.Door.push(portal.right, h)
     end
+    portal
   end
+end
 
-  def push_left(%Portal{left: left, right: right}) do
-    case Portal.Hole.pop(right) do
-      :error   -> :ok
-      {:ok, h} -> Portal.Hole.push(left, h)
-    end
-  end
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door  = inspect(left)
+    right_door = inspect(right)
 
-  def print(%Portal{left: left, right: right}) do
-    data_left  = inspect(Portal.Hole.get(left))
-    data_right = inspect(Portal.Hole.get(right))
+    left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
 
-    transfer = "    #{data_left} <=> #{data_right}    "
-    left     = inspect(left)
-    right    = String.rjust(inspect(right), String.length(transfer))
+    max = max(String.length(left_door), String.length(left_data))
 
-    IO.write """
-    #{left}
-
-    #{transfer}
-
-    #{right}
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
     """
   end
 end
---
+++ lib/portal/door.ex
@@ -0,0 +1,38 @@
+defmodule Portal.Door do
+  @doc """
+  Starts a door with the given `color`.
+
+  The color is given as a name so we can identify
+  the door by color name instead of using a PID.
+  """
+  def start_link(color) do
+    Agent.start_link(fn -> [] end, name: color)
+  end
+
+  @doc """
+  Get the data currently in the `door`.
+  """
+  def get(door) do
+    Agent.get(door, fn list -> list end)
+  end
+
+  @doc """
+  Push `value` into the door.
+  """
+  def push(door, value) do
+    Agent.update(door, fn list -> [value|list] end)
+  end
+
+  @doc """
+  Pops a value from the `door`.
+
+  It returns `{:ok, value}` if there is a value
+  or `:error` if the whole is currently empty.
+  """
+  def pop(door) do
+    Agent.get_and_update(door, fn
+      []    -> {:error, []}
+      [h|t] -> {{:ok, h}, t}
+    end)
+  end
+end
