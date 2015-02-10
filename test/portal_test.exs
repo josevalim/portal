87dfc82f430e6eab4af863d751fa951c961587f7
Laszlo Bacsi <lackac@lackac.hu>
2015-02-10 14:42:59 +0100

    Add meaningful tests

--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,7 +1,36 @@
 defmodule PortalTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "shooting a door" do
+    Portal.shoot :orange
+    assert Portal.Door.get(:orange), []
+  end
+
+  test "creating a transfer" do
+    create_transfer :orange, :blue, [1,2,3]
+    assert Portal.Door.get(:orange), [1,2,3]
+    assert Portal.Door.get(:blue), []
+  end
+
+  test "pushing right" do
+    portal = create_transfer :orange, :blue, [1,2,3]
+    Portal.push_right(portal)
+    assert Portal.Door.get(:orange), [1,2]
+    assert Portal.Door.get(:blue), [3]
+  end
+
+  test "pushing left" do
+    portal = create_transfer :orange, :blue, [1,2,3]
+    Portal.push_right(portal) # [1,2] / [3]
+    Portal.push_right(portal) # [1] / [2,3]
+    Portal.push_left(portal)  # [1,2] / [3]
+    assert Portal.Door.get(:orange), [1,2]
+    assert Portal.Door.get(:blue), [3]
+  end
+
+  defp create_transfer(source, target, list) do
+    Portal.shoot source
+    Portal.shoot target
+    Portal.transfer source, target, list
   end
 end
