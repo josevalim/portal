f2d2928db1d30c7ea94ead230af2eabca09bec3f
Laszlo Bacsi <lackac@lackac.hu>
2015-02-10 14:48:51 +0100

    Add more metadata to the mix

--- mix.exs
+++ mix.exs
@@ -5,6 +5,15 @@ defmodule Portal.Mixfile do
     [app: :portal,
      version: "0.0.1",
      elixir: "~> 1.0.0",
+     name: "Portal",
+     description: "A shooting fault-tolerant doors for distributed portal data-transfer application in Elixir",
+     package: [
+       contributors: ["José Valim", "László Bácsi"],
+       licenses: ["MIT"],
+       links: %{github: "https://github.com/josevalim/portal"}
+     ],
+     source_url: "https://github.com/josevalim/portal",
+     homepage_url: "http://howistart.org/posts/elixir/1",
      deps: deps]
   end
 
